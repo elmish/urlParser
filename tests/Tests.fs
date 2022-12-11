74a70dcc771c6688806d0994c861f8cd415595df
theor <theo.richart@gmail.com>
2022-12-11 13:33:20 -0500

    Update Tests.fs

--- tests/Tests.fs
+++ tests/Tests.fs
@@ -7,7 +7,8 @@ open Swensen.Unquote
 open Elmish.UrlParser
 
 type Routes =
-  IntAndTwoStringOptions of int * string option * string option
+  | IntAndTwoStringOptions of int * string option * string option
+  | Remaining of string
 
 let curry3 f a b c = f (a,b,c)
 
@@ -16,3 +17,8 @@ open NUnit.Framework
 let ``parses subroute and multiple params`` () =
   let parser = s "id" </> i32 <?> stringParam "one" <?> stringParam "two" |> map (curry3 IntAndTwoStringOptions)
   parseUrl parser "id/123?one=&two=" =! Some (IntAndTwoStringOptions (123,Some "",Some ""))
+
+[<Test>]
+let ``parses subroute and remaining url`` () =
+  let parser = s "id" </> remaining |> map Remaining
+  parseUrl parser "id/cats/and/dogs?one=one&two=two" =! Some (Remaining "cats/and/dogs")
